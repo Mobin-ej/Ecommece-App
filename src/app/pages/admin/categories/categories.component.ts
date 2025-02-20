446296ec79bd78f0c0e59dbfa9525a524a6ee126
mohammadreza82 <Jasmhidi.m.1382@gmail.com>
2025-02-20 11:52:29 +0330

    all

--- src/app/pages/admin/categories/categories.component.ts
+++ src/app/pages/admin/categories/categories.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../../services/product/product.service';
+import { ICategory } from '../../../types';
+import { map, Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-categories',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss'
 })
 export class CategoriesComponent {
+  // categoryList: ICategory[]=[]
+  products$:Observable<any>
+  constructor(private productService: ProductService) { 
+    this.products$ = productService.getAllCategory().pipe(
+      map((item: any) => {
+        console.log(item);
+        console.log(JSON.stringify(item));
+        
+        return item
+      })
+    )
+  }
 
+  // getAllCategories = () => {
+  //   this.productService.getAllCategory().subscribe((result: any) => {
+  //     console.log(result); 
+  //      this.categoryList=result
+  //   })
+  // }
 }
